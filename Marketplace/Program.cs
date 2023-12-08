2510e3c4b949675ff0c9e586647e5ceb54bf9c85
Yanainfotech <mayur.sharma93@gmail.com>
2023-12-08 11:09:56 +0530

    Test

--- Marketplace/Program.cs
+++ Marketplace/Program.cs
@@ -14,6 +14,7 @@ namespace Marketplace
         [STAThread]
         static void Main()
         {
+            IronXL.License.LicenseKey = "IRONXL-1782-139633-5ADF524-5189960C0-51DFC7-NEx7984-3927";
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
